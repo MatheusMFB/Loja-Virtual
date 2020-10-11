738ca4f0f7f8467934795039b75f4b4c4340dfce
Matheus Ferreira <musicmfb38@gmail.com>
2020-10-11 16:30:33 -0300

    Loja-Virtual
    update arquivos localhost

---
+++ android/app/src/main/kotlin/br/com/ferreira/loja_virtual_pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ferreira.loja_virtual_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// ignore: unused_import
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_pro/models/user_manager.dart';
+import 'package:loja_virtual_pro/screens/base/base_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => UserManager(),
+      child: MaterialApp(
+        title: 'Loja do Matheus',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 4, 125, 141),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: BaseScreen(),
+      ),
+    );
+  }
+}
